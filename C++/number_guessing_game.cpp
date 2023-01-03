43f117292fd039d044fae08b13630a2b321708bb
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2023-01-03 00:04:57 -0600

    Update number_guessing_game.cpp

--- C++/number_guessing_game.cpp
+++ C++/number_guessing_game.cpp
@@ -1,4 +1,4 @@
-//using C++
+//Using C++
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -14,7 +14,7 @@ int main()
 	srand(time(0));
 	answer = (rand() % MAX) + MIN; 
 	
-    cout << "Guess the number between 1 and 100." << endl;
+    	cout << "Guess the number between 1 and 100." << endl;
 	cout << "Guess the number: ";
 
 	do {
