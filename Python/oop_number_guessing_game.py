c0d8c26a6ab6860dabf37815f78a69b095b25d64
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2023-01-02 23:40:50 -0600

    Update oop_number_guessing_game.py

--- Python/oop_number_guessing_game.py
+++ Python/oop_number_guessing_game.py
@@ -9,17 +9,17 @@ class Guess:
         self.start_game = "Guess the number between 1 and 100."
         self.first_try = "Guess the number: "
         self.new_try = "Try again: "
-        self.too_low = "Your number is too low."
-        self.too_high = "Your number is too high."
+        self.low = "Your number is too low."
+        self.high = "Your number is too high."
         self.correct = "You guessed it right!!"
 
     def too_low(self):
-        print(self.too_low)
+        print(self.low)
         guess = int(input(self.new_try))
         return guess
 
     def too_high(self):
-        print(self.too_high)
+        print(self.high)
         guess = int(input(self.new_try))
         return guess
 
