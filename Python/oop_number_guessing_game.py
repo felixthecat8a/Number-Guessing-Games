03a8c2f2a71610c870dd585520f706f51c8e9b1a
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2023-01-06 02:12:03 -0600

    Update oop_number_guessing_game.py

--- Python/oop_number_guessing_game.py
+++ Python/oop_number_guessing_game.py
@@ -2,8 +2,7 @@
 import random
 
 class Guess:
-    def __init__(self,number):
-        self.answer = int(number)
+    def __init__(self):
         self.attempts = 1
         self.start_game = "Guess the number between 1 and 100."
         self.first_try = "Guess the number: "
@@ -11,10 +10,22 @@ class Guess:
         self.low = "Your number is too low."
         self.high = "Your number is too high."
         self.correct = "You guessed it right!!"
-        
+
     def start(self):
         self.answer = int(random.randrange(1,100))
-        #print("the number is {:d}" .format(self.answer)) #for debugging
+        print("the number is {:d}" .format(self.answer)) #for debugging
+        print(self.start_game)
+        self.guess = int(input(self.first_try))
+
+    def check(self):
+        while self.answer!= self.guess:
+            self.attempts = self.attempts + 1
+            if self.guess < self.answer: 
+                self.guess = self.too_low()
+            elif self.guess > self.answer: 
+                self.guess = self.too_high()
+            else: break
+        self.win()
 
     def too_low(self):
         print(self.low)
@@ -27,40 +38,31 @@ class Guess:
         return guess
 
     def win(self):
-        print("You guessed it right!!")
+        print(self.correct)
         print(f"It only took you {self.attempts} {self.tries()}. :)")
 
     def tries(self):
         if self.attempts == 1:
-            return " try"
+            return "try"
         else:
-            return" tries"
+            return"tries"
 
     def game(self):
-        self.start()
-        print(self.start_game)
-        self.guess = int(input(self.first_try))
-        while self.answer!= self.guess:
-            self.attempts = self.attempts + 1
-            if self.guess < self.answer: 
-                self.guess = self.too_low()
-            elif self.guess > self.answer: 
-                self.guess = self.too_high()
-            else: break
-        self.win()
+        while(True):
+            self.start()
+            self.check()
+            prompt_message = "Do you want to play again? (y/n): "
+            yes_or_no = input(prompt_message).lower()
+            if yes_or_no == "yes" or yes_or_no == "y":
+                self.attempts = 1
+                continue
+            elif yes_or_no == "no" or yes_or_no == "n":
+                print("Goodbye :)")
+                break
+            else: 
+                print('Invalid Response - Exiting Game')
+                break
 
 if __name__ == '__main__':
-    active = True
-    while(active):
-        play = Guess()
-        play.game()
-        prompt_message = "Do you want to play again? (y/n): "
-        yes_or_no = input(prompt_message).lower()
-        if yes_or_no == "yes" or yes_or_no == "y":
-            continue
-        elif yes_or_no == "no" or yes_or_no == "n":
-            print("Goodbye :)")
-            break
-        else: 
-            print('Invalid Response - Exiting Game')
-            break
+    play = Guess()
+    play.game()
