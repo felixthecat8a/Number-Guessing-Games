c69a26040a6926532982d814429184dfa9fc6093
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2023-01-05 23:18:08 -0600

    Update number_guessing_game.js

--- Javascript/number_guessing_game.js
+++ Javascript/number_guessing_game.js
@@ -1,9 +1,14 @@
 //using Node.js
 //npm install prompt-sync
+
 const prompt = require('prompt-sync')({sigint: true});
+
 const randomNumber = Math.floor(Math.random() * 100) + 1
+console.log(`Answer: ${randomNumber}`) //for debugging
+
 let attempts = 0
 let matching = false
+
 console.log("Guess the number between 1 and 100.")
 let guess = prompt("Guess the number: ")
 do {
@@ -16,7 +21,15 @@ do {
         guess = prompt("Try again: ")
     } else {
         console.log("You guessed it right!</div>")
-        console.log(`It only took you ${attempts} tries.`)
+        console.log(`It only took you ${attempts} ${tries(attempts)}.`)
         matching = true
     }
 } while (!matching)
+
+function tries(attempts) {
+    if (attempts === 1) {
+        return "try"
+    }else{
+        return "tries"
+    }
+}
