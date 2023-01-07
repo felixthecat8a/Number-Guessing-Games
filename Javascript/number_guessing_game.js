66bc4aadfc1a9dc48743278286f7e043066f2c5f
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2023-01-07 08:43:34 -0600

    Update number_guessing_game.js

--- Javascript/number_guessing_game.js
+++ Javascript/number_guessing_game.js
@@ -7,7 +7,6 @@ const randomNumber = Math.floor(Math.random() * 100) + 1
 console.log(`Answer: ${randomNumber}`) //for debugging
 
 let attempts = 0
-let matching = false
 
 console.log("Guess the number between 1 and 100.")
 let guess = prompt("Guess the number: ")
@@ -19,13 +18,12 @@ do {
     } else if (guess > randomNumber) {
         console.log("Your number is too high.")
         guess = prompt("Try again: ")
-    } else {
-        console.log("You guessed it right!</div>")
-        console.log(`It only took you ${attempts} ${tries(attempts)}.`)
-        matching = true
-    }
-} while (!matching)
+    } 
+} while (guess != randomNumber)
 
+console.log("You guessed it right!</div>")
+console.log(`It only took you ${attempts} ${tries(attempts)}.`)
+    
 function tries(attempts) {
     if (attempts === 1) {
         return "try"
