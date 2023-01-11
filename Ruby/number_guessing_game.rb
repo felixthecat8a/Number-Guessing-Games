27fe50a663e8f9d0c6b029eae06cc7efaa006d80
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2023-01-10 21:14:52 -0600

    Add files via upload

---
+++ Ruby/number_guessing_game.rb
@@ -0,0 +1,39 @@
+#using Ruby
+#to run: ruby number_guessing_game.rb
+debug = true
+puts "Guess the number between 1 and 100."
+
+number = rand(100)
+if debug
+    puts "Answer: #{number}"
+end
+
+puts "Guess the number: "
+guess = gets.chomp.to_i
+
+attempts = 1
+
+while guess != number
+    attempts +=1
+    if guess < number
+        puts "Your number is too low."
+        puts "Try again: "
+        guess = gets.chomp.to_i
+    elsif guess > number
+        puts "Your number is too high."
+        puts "Try again: "
+        guess = gets.chomp.to_i
+    else
+        break
+    end
+end
+
+puts "You guessed it right!"
+
+if attempts == 1
+    tries = "attempt"
+else
+    tries = "attempts"
+end
+
+puts "It only took you #{attempts} #{tries}. :)"
