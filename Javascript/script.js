3dc156232d735d8bb2234b0ff8872ad299f14e95
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2023-02-03 00:07:08 -0600

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,6 @@
 const check = document.getElementById("check")
 const guess = document.getElementById("guess")
 const result = document.getElementById("result")
-const attempt = document.getElementById("attempt")
 const reset = document.getElementById("reset")
 
 
@@ -13,14 +12,11 @@ function playGame() {
   function guessNumber() {
       attempts++
       if (guess.value < randomNumber) {
-          result.innerHTML = "<div class='low'>Your number is too low. Try again.</div>"
-          attempt.innerHTML = `<h3>Attempt number ${attempts}.</h3>`
+          result.innerHTML = `<div style='color:cornflowerblue'>Attempt number ${attempts} was too low. Try again.</div>`
       } else if (guess.value > randomNumber) {
-          result.innerHTML = "<div class='high'>Your number is too high. Try again.</div>"
-          attempt.innerHTML = `<h3>Attempt number ${attempts}.</h3>`
+          result.innerHTML = `<div style='color:palevioletred'>Attempt number ${attempts} was too high. Try again.</div>`
       } else {
-          result.innerHTML = "<div class='right'>You guessed it right!</div>"
-          attempt.innerHTML = `<h3>It only took ${attempts} ${attemptsMessage()}.</h3>`
+          result.innerHTML = `<span style='color:gold'>You guessed it right!</span><br><br>It only took you ${attempts} ${tries()}.`
           reset.innerHTML = "<button type='reset' onclick='resetGame()'>Play Again?</button>" 
       }
   }
@@ -37,7 +33,6 @@ playGame()
 
 function resetGame() {
     result.innerHTML = "<h3 id='result'>Guess the number.</h3>"
-    attempt.innerHTML = "<div id='attempt'></div>"
     reset.innerHTML = "<div id='reset'></div>" 
     playGame()
 }
