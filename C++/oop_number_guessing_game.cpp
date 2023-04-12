460d61bd4ae2fbbc4337c533cf843232244a4f48
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2023-04-11 23:33:28 -0500

    Update oop_number_guessing_game.cpp

--- C++/oop_number_guessing_game.cpp
+++ C++/oop_number_guessing_game.cpp
@@ -1,46 +1,46 @@
 //Using C++
 #include <iostream> // for std::cout, std::cin & std::streamsize
 #include <string> // for std::string & std::to_string()
-#include <cstdlib> // for rand() & srand()
-#include <ctime> // for time()
-using namespace std;
-
-#define MIN 1
-#define MAX 100
+#include <limits> // for std::numeric_limits
+#include <cctype> // for tolower()
+#include <random> // for random number generation
+#include <chrono> // for high_resolution_clock
 
 class NumberGuessingGame {
 	private:
+	bool debug = false; //set true to debug
+	const int MIN = 1;
+	const int MAX = 100;
 	int randNum, guessNum;
-	int attempts = 0;
+	int attempts = 1; //start counting after first try
 	int maxAttempts = 10;
 	const std::string startIntro = std::to_string(MIN) + " and " + std::to_string(MAX);
 	const std::string attemptIntro = " in " + std::to_string(maxAttempts) + " attempts or less.";
 	const std::string gameIntro = "Guess the number between " + startIntro + attemptIntro;
-	const string firstGuess = "Guess the number: ";
-	const string tooLow = "Your number is too low.\nTry Again: ";
-	const string tooHigh = "Your number is too high.\nTry Again: ";
-	const string correct = "You guessed it right!!\n";
+	const std::string firstGuess = "Guess the number: ";
 	char playAgainInput;
-	bool debug = true; //set true to debug
 
 	public:
-	void createAnswer() {
-		srand(time(0));
-		randNum = (rand() % MAX) + MIN;
+	void createAnswer() { 
+		// Use high-resolution clock to generate a unique seed
+		auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+		std::mt19937 rng(seed); // Mersenne Twister random number engine
+		std::uniform_int_distribution<int> dist(MIN, MAX);
+		randNum = dist(rng); // generate random number
 		if (debug) {
-			cout << "Answer: " << randNum << endl;
+			std::cout << "\nAnswer: " << randNum << std::endl;
 		}
 	}
 
 	void startGame() {
-		cout << gameIntro << endl;
+		std::cout << gameIntro << std::endl;
 		createAnswer();
-		cout << firstGuess;
+		std::cout << firstGuess;
 		playGame();
 	}
 
 	void playGame() {
-		cin >> guessNum;
+		std::cin >> guessNum;
 		checkAnswer();
 		playAgain();
 	}
@@ -49,13 +49,12 @@ class NumberGuessingGame {
 		do {
 			checkLimit();
 			checkInput();
-			attempts++;
 			if (guessNum < randNum) {
-				cout << tooLow;
-				cin >> guessNum;
+				tooLOW();
+				continue;
 			} else if (guessNum > randNum) {
-				cout << tooHigh;
-				cin >> guessNum;
+				tooHIGH();
+				continue;
 			}
 		} while (guessNum != randNum);
 		win();
@@ -63,40 +62,55 @@ class NumberGuessingGame {
 
 	void checkLimit() {
 		if (attempts >= maxAttempts) {
-			string attemptPrompt = std::to_string(maxAttempts) + " tries.";
-			string limitPrompt = "Sorry, you've reached " + attemptPrompt;
-			cout << limitPrompt << endl;
-			cout << "The number was " << randNum << "." << endl;
+			std::string attemptPrompt = std::to_string(maxAttempts) + " tries. :(";
+			std::string limitPrompt = "Sorry, you've reached " + attemptPrompt;
+			std::cout << limitPrompt << std::endl;
+			std::cout << "The number was " << randNum << "." << std::endl;
 			playAgain();
 		}
 	}
 
 	void checkInput() {
 		while (std::cin.fail()) {
-			cout << "Invalid input. Please enter an integer.\nTry Again: ";
-			cin.clear();
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			cin >> guessNum;
+			std::cout << "Invalid input. Please enter an integer.\nTry Again: ";
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cin >> guessNum;
 		}
 	}
 
+	void tooLOW() {
+		std::string tooLow = "Your number is too low.\nTry Again: ";
+		std::cout << tooLow;
+		attempts++;
+		std::cin >> guessNum;
+	}
+
+	void tooHIGH() {
+		std::string tooHigh = "Your number is too high.\nTry Again: ";
+		std::cout << tooHigh;
+		attempts++;
+		std::cin >> guessNum;
+	}
+
 	void win() {
-		string tries;
+		std::string correct = "You guessed it right!!!\n";
+		std::string tries;
 		if (attempts == 1) {
 			tries = " attempt. :)";
 		} else {
 			tries = " attempts. :)";
 		}
-		cout << correct << "It only took " << attempts << tries << endl;
+		std::cout << correct << "It only took " << attempts << tries << std::endl;
 	}
 
 	void playAgain() {
-		string prompt = "Do you want to play again?\n(y/n): ";
+		std::string prompt = "Do you want to play again?\n(y/n): ";
 		int i = 0;
 		bool message = true;
 		while (i < 3) {
-			cout << prompt;
-			cin >> playAgainInput;
+			std::cout << prompt;
+			std::cin >> playAgainInput;
 			playAgainInput = tolower(playAgainInput);
 			if (playAgainInput == 'y') {
 				attempts = 0;
@@ -105,18 +119,18 @@ class NumberGuessingGame {
 				break;
 			}
 			else if (playAgainInput == 'n') {
-				cout << "Goodbye. :)" << endl;
+				std::cout << "Goodbye. :)" << std::endl;
 				message = false;
 				break;
 			} else {
-				cout << "Invalid Input. Please enter \"y\" or \"n\"." << endl;
+				std::cout << "Invalid Input. Please enter \"y\" or \"n\"." << std::endl;
 				i++;
 				message = true;
 				continue;
 			}
 		}
 		if (message) {
-			cout << "Too Many Invalid Inputs - Exiting Game" << endl;
+			std::cout << "Too Many Invalid Inputs - Exiting Game" << std::endl;
 			exit (EXIT_FAILURE);
 		}
 		exit (EXIT_SUCCESS);
