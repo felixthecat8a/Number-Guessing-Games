2a2d1ab019fcb9c4521f401011894073c9d92ef8
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2024-04-26 20:51:12 -0500

    Update number_guessing_game.py

--- Python/number_guessing_game.py
+++ Python/number_guessing_game.py
@@ -1,37 +1,32 @@
 #using Python
 import random
 
-debug = True #set True to debug
-
 start_game = "Guess the number between 1 and 100."
 print(start_game)
 
 number = random.randrange(1,100)
-if debug:
-    print("Answer: {:d}" .format(number)) 
+
+debug = True #set True to debug
+if debug: print("Answer: {:d}" .format(number)) 
 
 first_try = "Guess the number: "
 guess = int(input(first_try))
-
-new_try = "Try again: "
-too_low = "Your number is too low."
-too_high = "Your number is too high."
 
 attempts = 1
 
 while number!= guess:
     attempts = attempts + 1
     if guess < number:
-        print(too_low)
-    elif guess > number:
-        print(too_high)
-    guess = int(input(new_try))
+        print("Your number is too low.")
+    else:
+        print("Your number is too high.")
+    guess = int(input("Try again: "))
 
 if attempts == 1:
     tries = "attempt"
-else: 
+else:
     tries = "attempts"
 
-win_game = "You guessed it right!!"
-print(win_game)
+print("You guessed it right!!")
 print(f"It only took {attempts} {tries}. :)")
+#print("It only took {:d} {}. :)".format(attempts,tries))
